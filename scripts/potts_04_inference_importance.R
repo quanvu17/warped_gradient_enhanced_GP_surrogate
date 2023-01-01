d22f73a2bed0e3b6639c04638f7e45077cb96747
QuanVu <dmqv953@uowmail.edu.au>
2023-01-02 06:39:10 +1100

    Fix importance sampling code for Potts and autologistic models + Update results

--- scripts/potts_04_inference_importance.R
+++ scripts/potts_04_inference_importance.R
@@ -2,6 +2,41 @@
 
 # Load source
 source('scripts/utils.R')
+load("results/potts_sample.rda")
+
+beta <- sample_mean <- sample_var <- rep(0, length(sample_data))
+
+for (i in 1:length(sample_data)){
+  beta[i] <- sample_data[[i]][[1]]
+  sample_mean[i] <- sample_data[[i]][[2]]
+  sample_var[i] <- (sample_data[[i]][[3]])^2
+}
+
+df_all <- data.frame(beta = beta, mean = sample_mean, var = sample_var)
+
+df_all$noise_var_m <- df_all$var / 500
+df_all$noise_var_v <- 2 * df_all$var^2 / 499
+
+df_test <- df_all[2*(1:50),]
+df <- setdiff(df_all, df_test)
+
+mean1 <- mean(df$mean)
+df$mean <- df$mean - mean1
+
+# Fit surrogate model
+
+# Gaussian process
+
+layers <- c(AWU(r = 100L, dim = 1L, grad = 250, lims = c(-0.5, 0.5)))
+
+d_GP <- surrogate_GP(f = mean + var ~ beta - 1, data = df, layers = layers,
+                     model = "meanvar",
+                     method = "ML", nsteps = 200L,
+                     noise_var_m = df$noise_var_m, noise_var_v = df$noise_var_v
+)
+
+##############################
+
 registerDoParallel(cores = detectCores()) ## 64L
 
 # Initialize parameters
@@ -24,9 +59,8 @@ maxS <- nrow(edges)
 ############## GP
 newdata <- data.frame(beta = seq(1.16, 1.18, length.out = 1001))
 
-i = commandArgs(trailingOnly=TRUE)
-i <- as.numeric(i)
-load(paste0('results/potts_simulated_data_', i, '.rda'))
+for (j in 1:5){
+load(paste0('results/potts_simulated_data_', j, '.rda'))
 
 syn_like_GP <- synthetic_likefnc_GP_1D_sample(d_GP, newdata, summary_stats, mean1)
 syn_like <- rowMeans(exp(syn_like_GP[[4]]), na.rm = T)
@@ -59,9 +93,11 @@ for (i in 1:length(beta)){
   summary_stats_sim[i] <- sample_data[[i]][[2]]
 }
 
-log_weights <- summary_stats * beta + summary_stats_sim * beta_hat - summary_stats_sim * beta - syn_like
+log_weights <- summary_stats * beta + 
+  summary_stats_sim * beta_hat - summary_stats_sim * beta - log(syn_like)
 weights <- exp(log_weights - max(log_weights))
 weights_norm <- weights / sum(weights)
 posterior_GP <- list(beta, weights_norm, time, time0)
 
-save(posterior_GP, file = paste0('results/potts_importance_', i, '.rda'))
+save(posterior_GP, file = paste0('results/potts_importance_', j, '.rda'))
+}
